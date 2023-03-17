d2c6e7421614e3917762d10b50793d00d2732789
Brandon St. Louis <112421430+boopa1013@users.noreply.github.com>
2023-03-17 12:58:49 -0400

    my version

--- app/base/urls.py
+++ app/base/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("LogIn/",views.LogIn, name="LogIn" )
 ]
--- app/base/views.py
+++ app/base/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "home.html")
+def LogIn(request):
+    return render(request, "LogIn.html")
+def Contact(request):
+    return render(request,"Contact.html")
