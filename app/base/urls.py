665479c6ad6946fdd13a644e3e0ba18cc6b3d0ad
kbak8 <kevbakjm@gmail.com>
2023-03-31 15:22:55 -0400

    Translator almost working

--- app/base/urls.py
+++ app/base/urls.py
@@ -6,8 +6,5 @@ urlpatterns = [
     path("login/",views.login, name="login"),
     path("help/", views.help, name="help"),
     path("contact/", views.contact, name="contact"),
-    path("Registration/", views.registration,name="registration")
-    path('run-script/', views.run_script, name='run_script'),
-    path('script/', views.script, name='script'),
-
+    path("registration/", views.registration, name="registration"),
 ]
