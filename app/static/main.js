6b3b19dc05e40757b5fa9ca1ed99f2d7f3355da9
Caden-Kienitz <cdkienit@mtu.edu>
2023-04-05 13:52:26 -0400

    Revert "It works"
    This reverts commit d1cd88439e22195047214234b42c5b4b423f8cff.

--- app/static/main.js
+++ app/static/main.js
@@ -1,55 +1,75 @@
 let IPA_result = "";
-let data_file = 'static/en_us.json';
+let data_file = '/static/en_us.json';
 
-function update_result() {
-  let c_w = (get_IPA_tBox() + " ").split(" ");
+function update_result () {
+  
+  let c_w = (get_IPA_tBox()+" ").split(" ");
 
-  set_IPA_tBox("loading....");
+  set_IPA_tBox ("loading....");
 
-  get_IPA_DB(IPA_db => {
-      let str = "";
-      for (let i = 0; i < c_w.length; i++) {
-          let word = c_w[i].toLowerCase().replace(/[^\w\s]/gi, '');
-          if (word != "") {
-              if (IPA_db[word]) {
-                  str += IPA_db[word] + " ";
-              } else {
-                  str += word + " ";
-              }
-          }
-      }
-      set_IPA_tBox(str);
+  get_IPA_DB ((obj)=>{
+
+    
+    let str = "";
+
+    for (var i = 0; i < c_w.length; i++) {
+
+      let word = c_w[i];
+
+      preprocess_eng(word,(t_word)=>{
+
+        if ( word != "") {
+          if(typeof obj[t_word] != "undefined"){
+
+            let ipa = obj[t_word];
+
+            if (document.getElementById("wf_c_words").checked) {
+
+                str += "( " + word + " : " + ipa + " ) ";
+
+              }else  str += ipa + " ";
+
+          }else str += word + " ";
+
+          set_IPA_tBox (str);
+
+        }
+      });
+    }
+
   });
 }
 
-
-function get_IPA_DB(callback) {
-  console.log('get_IPA_DB called'); // Add this line
+function get_IPA_DB (s) {
   var xmlhttp = new XMLHttpRequest();
   xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      var myObj = JSON.parse(this.responseText);
-      var IPA_db = myObj['en_US'][0]; // access the IPA database for en_US
-      callback(IPA_db); // call the callback function with the IPA_db value
-    }
+      if (this.readyState == 4 && this.status == 200) {
+          var myObj = JSON.parse(this.responseText);
+          return s(myObj);
+      }
   };
 
-  xmlhttp.open("GET", "static/en_us.json", true);
+  if (document.getElementById("IPA_US").checked){
+    data_file = '/static/en_us.json';
+  }else if (document.getElementById("IPA_UK").checked) {
+    data_file = '/static/en_us.json';
+  }else {
+    data_file = '/static/en_us.json';
+  }
+
+  xmlhttp.open("GET", data_file, true);
   xmlhttp.send();
 }
-
-
 
 function get_IPA_tBox () {
   return document.getElementById("cWords_tBox").value
 }
 
 function set_IPA_tBox (v = IPA_result) {
-  console.log('set_IPA_tBox called with value:', v); // Add this line
   document.getElementById("IPA_tBox").value = v;
 }
 
-/*function preprocess_eng (x,callback){
+function preprocess_eng (x,callback){
   x = x.replace(/A/g, "a");
   x = x.replace(/B/g, "b");
   x = x.replace(/C/g, "c");
@@ -80,6 +100,6 @@ function set_IPA_tBox (v = IPA_result) {
   x = x.replace(/\,/g, "");
   x = x.replace(/\n/g, "");
   callback(x);
-}*/
+}
 
 update_result ();
